299820d740e7be8e5328b52609a59c116d020414
Liv Selle Underhaug <liv.underhaug@bouvet.no>
2022-09-15 10:01:07 +0200

    change in drawings

--- src/Bouvet.AssetHub.API/Entities/TestDatabaseSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace Bouvet.AssetHub.API.Entities 
-{
-    public class TestDatabaseSettings
-    {
-        public string ConnectionString  {get; set;} = null!;
-        public string DatabaseName {get; set;} = null!;
-        public string CollectionName {get; set;} = null!;
-    }
-}
--- src/Bouvet.AssetHub.API/Program.cs
+++ src/Bouvet.AssetHub.API/Program.cs
@@ -1,25 +1,11 @@
-using Bouvet.AssetHub.API.Data;
-using Bouvet.AssetHub.API.Data.Interfaces;
-using Bouvet.AssetHub.API.Entities;
-using Bouvet.AssetHub.API.Repository;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.Configure<TestDatabaseSettings>(
-    builder.Configuration.GetSection("DatabaseSettings")
-);
-
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-
-builder.Services.AddScoped<ITestContext, TestContext>();
-builder.Services.AddScoped<IAssetRepository, AssetRepository>();
 
 var app = builder.Build();
 
